8287beff7bc9b9e40a8959ba0ba511370b704dba
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-18 06:50:12 +0530

    Adding keyup event

---
+++ GitHub Finder/js/app.js
@@ -0,0 +1,13 @@
+const textI = document.querySelector('#searchUser');
+
+textI.addEventListener('keyup',e=>{
+
+   const input = textI.value;
+
+   if(input !== '')
+   {
+    // console.log(input);
+   }
+
+
+})
