c99d12d1c842a13de41172d6e73b086c92d02412
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-20 01:27:17 +0530

    Adding The Fetch

--- Name Generator/js/app.js
+++ Name Generator/js/app.js
@@ -1,6 +1,6 @@
 // variables 
 const submitBtn = document.querySelector('#view');
-const resultDiv = document.querySelector('#result');
+const resultDiv = document.querySelector('#rows');
 const country = document.querySelector('#country');
 const genre = document.querySelector('#genre');
 const no = document.querySelector('#quantity');
@@ -17,12 +17,22 @@ function viewNames(e)
     const nValue = no.value;
     if(cValue === '' || gValue ==='' || nValue === '')
     {
-          alert('Enter values...');
+          alert('All Fields Are Required');
     }
     else
     {
-     
-        
+        resultDiv.innerHTML = '';
+       fetchTheData()
+       .then(data=>
+        {
+            resultDiv.innerHTML = `
+              <tr>${name.name}</<tr>
+              <tr>${name.gender}</<tr>
+              <tr>${name.region}</<tr>
+            
+            `;
+        })
+       .catch(()=>console.log('There is error '))
        console.log('The Values ' + cValue + ' ' + gValue + ' '+ nValue);
     }
    
@@ -30,5 +40,16 @@ function viewNames(e)
     
     
     e.preventDefault();
+}
+
+// fetch data 
+async function fetchTheData(country,gender,number)
+{
+    
+    const profileRespose = await fetch(`https://uinames.com/api/?region=${country}&gender=${gender}&amount=${number}`,{mode:'no-cors'}) 
+    const repo = await profileRespose.json();
+  
+   return {repo}
+
 }
 
