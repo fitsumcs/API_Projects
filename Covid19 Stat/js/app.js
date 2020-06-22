da6376bc133d9096776e6d13177abf5ba17b0478
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-22 22:57:01 +0530

    Initializing Covid Api Project and UI Done

---
+++ Covid19 Stat/js/app.js
@@ -0,0 +1,36 @@
+const textI = document.querySelector('#searchUser');
+const gitH = new GitHub();
+const ui = new UI();
+textI.addEventListener('keyup',e=>{
+
+   const input = textI.value;
+
+   if(input !== '')
+   {
+    // console.log(input);
+    // make the class 
+    gitH.getUser(input).then(data=>{
+       
+        if(data.profile.message === 'Not Found')
+        {
+            // show error
+            ui.showError('User Does Not Exist', 'alert alert-danger');
+            
+        }
+        else
+        {
+            // show profile
+            ui.showProfile(data.profile);
+            ui.showRepo(data.repo);
+            // console.log(data);
+        }
+    });
+   }
+   else
+   {
+    //    clear profile
+        ui.clearProfile();
+   }
+
+
+})
