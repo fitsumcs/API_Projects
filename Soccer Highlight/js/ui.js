21e5080e988a5f4baf170ffcb59eaeec9ecaa7e8
Fitsum Alemu <fitsumcs@gmail.com>
2020-06-24 22:00:14 +0530

    English prem done

--- Soccer Highlight/js/ui.js
+++ Soccer Highlight/js/ui.js
@@ -9,30 +9,31 @@ class SoccerUI {
     showSoccerData(data) 
     {
         let output = '';
-        // console.log(data.data);
+         console.log(data.data);
        data.data.forEach(element => 
         {
          if(element.competition.name.includes('ENGLAND'))
          {
            output +=`
            
-           <div class="col-sm-6  mb-3">
-           <div class="card mb-3 h-100">
+           <div class="col-sm-6   mb-3">
+           <div class="card mb-3 d-flex align-items-stretch">
 
                <div class="card-body">
-                  <p class="card-text"><vedio>${element.embed}</vedio></p>
-                   <h5 class="card-title">${element.title}</h5>
+                  <div class="card-img-top">${element.videos[0].embed}</div>
+                   <h5 class="card-title mt-3">${element.title}</h5>
                    <p class="card-text"><small class="text-muted">${element.competition.name}</small></p>
                    <p class="card-text"><small class="text-muted">Date: ${element.date}</small></p>
                 </div>
            </div>
        </div>
+      
        
          
            
            
            `;
-           console.log(element.embed);
+          //  console.log(element.embed);
          }
             // console.log(element.competition.name);
             // console.log(element.date);
